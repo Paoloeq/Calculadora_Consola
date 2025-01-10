1ba66553f1d42bdf5942c849aef6a3912db9f1d6
Paolo <paoloquispe.quiroz@gmail.com>
2025-01-10 08:54:48 -0500

    change value erro try catch

--- main.py
+++ main.py
@@ -3,34 +3,44 @@ def Calculadora():
     print("Bienvenido a la calculadora en python :D")
 
     # Pedimos los números
-    num1 = float(input("Ingresa el número 1: "))
-    num2 = float(input("Ingrese el número 2: "))
-    
-    # Elegir la operación
-    print("Selecciona la operación: ")
-    print("1. Suma")
-    print("2. Resta")
-    print("3. Multiplicación")
-    print("4. División")
-    operacion = input("Ingresa el número de la operación (1/2/3/4)")
+    try:
+        num1 = float(input("Ingresa el número 1: "))
+        num2 = float(input("Ingrese el número 2: "))
 
-    # Calcular el Resultado
-    if operacion == "1":
-        resultado = num1 + num2
-        print(f"El resultado de la operación es : {resultado}")
-    elif operacion == "2":
-        resultado = num1 - num2
-        print(f"El resultado de la operación es: {resultado}")
-    elif operacion == "3":
-        resultado = num1 * num2
-        print(f"El resultado de la operación es: {resultado}")
-    elif operacion == "4":
-        if num2!=0:
-            resultado = num1 / num2
+        # Elegir la operación
+        print("Selecciona la operación: ")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicación")
+        print("4. División")
+        operacion = input("Ingresa el número de la operación (1/2/3/4)")
+
+        # Calcular el Resultado
+        if operacion == "1":
+            resultado = num1 + num2
+            print(f"El resultado de la operación es : {resultado}")
+        elif operacion == "2":
+            resultado = num1 - num2
             print(f"El resultado de la operación es: {resultado}")
+        elif operacion == "3":
+            resultado = num1 * num2
+            print(f"El resultado de la operación es: {resultado}")
+        elif operacion == "4":
+            if num2 != 0:
+                resultado = num1 / num2
+                print(f"El resultado de la operación es: {resultado}")
+            else:
+                print("no se puede dividir entre cero")
         else:
-            print("no se puede dividir entre cero")
-    else: print("Operación no valida intenta de nuevo")
+            print("Operación no valida intenta de nuevo")
+
+    except ValueError:
+        print("Error: Por favor Ingresa un número válido")
 
 # Llamar a la función
-Calculadora()
+while True:
+    Calculadora()
+    continuar = input("Quieres realizar otra operación? (s/n)").lower()
+    if continuar != "s":
+        print("Gracias por usar la calculadora, hasta la próxima")
+        break
